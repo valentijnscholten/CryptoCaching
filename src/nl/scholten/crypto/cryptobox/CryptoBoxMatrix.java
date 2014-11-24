9ff4b0bfe04d30b273706672017bfe1e49768e96
valentijnscholten <valentijnscholten@gmail.com>
2014-11-24 21:57:57 +0100

    [PERF] Make mandatory to start with a hit -> 10x better performance.

--- src/nl/scholten/crypto/cryptobox/CryptoBoxMatrix.java
+++ src/nl/scholten/crypto/cryptobox/CryptoBoxMatrix.java
@@ -21,13 +21,21 @@ public abstract class CryptoBoxMatrix extends Matrix {
 	@Override
 	protected int score() {
 		int result = 0;
+		//first see if the first characters are a hit, if not, score is 0.
+		boolean prematch = false;
+		for (String hit: hits) {
+			prematch = data.startsWith(hit);
+			if (prematch) break;
+		}
+		if (!prematch) return 0;
+		
 		for (String hit: hits) {
 			int count = StringUtils.countMatches(data, hit);
+//			int count = StringUtils.indexOf(data, hit) / 1; // doesn't help much
 			//extra score for first occurrence to stimulate more different words
 			if (count > 0) result += 10; 
 			result += count;
 		}
-//		System.out.println("Score = " + result + " for " + plain);
 		return result;
 	}
 
